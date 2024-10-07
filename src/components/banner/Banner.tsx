3257a478cdc66c9487f0ae83690b6af2d7c6ad88
acoria <tewes.verena@gmail.com>
2024-10-07 16:08:09 +0200

    Add contact button

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -1,6 +1,14 @@
 import { IBannerProps } from "./IBannerProps";
 import styles from "./Banner.module.scss";
+import { useTranslation } from "../../hooks/useTranslation/useTranslation";
+import { texts } from "../../hooks/useTranslation/texts";
 
 export const Banner: React.FC<IBannerProps> = (props) => {
-  return <div className={styles.banner}>{props.children}</div>;
+    const {t} = useTranslation()
+  return (
+    <div className={styles.banner}>
+      {props.children}
+      <button className={styles.contactButton}>{t(texts.contact)}</button>
+    </div>
+  );
 };
